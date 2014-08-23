c285ba483a5d539ea8d50eb5852919f25aec0e65
iuli1478 <iuli.iuli17@gmail.com>
2014-08-24 01:13:48 +0300

    Upload

---
+++ UploadTest/uf.php
@@ -0,0 +1,49 @@
+<html>
+<head >
+	<meta http-equiv="content-type" content="text/html; encoding=utf-8" />
+	<title>Upload file to server demo</title>
+</head>
+<body >
+	<div style="background-color:lightgray; border-width:1px; border-style:dashed; padding:20px;"
+	<?php
+		require("uf_cfg.php");
+
+		function raise_upload_error($error_msg) {
+			die("Error: ".$error_msg);
+		}
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			sleep(1);
+			
+			if (!isset($_FILES["upfile"]["error"]))
+				raise_upload_error("Upload error");
+			if ($_FILES["upfile"]["error"] > 0)
+				raise_upload_error($_FILES["upfile"]["error"]);
+				
+			if (is_uploaded_file($_FILES['upfile']['tmp_name'])) {
+				move_uploaded_file($_FILES['upfile']['tmp_name'], $upload_path.$upload_slash.$_FILES['upfile']['name']);
+				
+				echo "&nbsp; <br/>";
+				$filesize = $_FILES['upfile']['size'];
+				if($filesize == 0 || $filesize > ($max_file_size*1024) ) {
+					raise_upload_error( "File size must be under ".($max_file_size)." KB !!! <br/>".
+										"Current filesize = ".round($filesize/1024)." KB  <br/>");
+				}
+				
+				echo "File <b>". $_FILES['upfile']['name'] ."</b> uploaded successfully.\n<br>";
+				echo "Type: ".$_FILES['upfile']['type']."<br/>";			
+				echo "Filesize:  ".$filesize." bytes / ".round($filesize/1024)." KB / ".round(($filesize/1024)/1024)." MB \n<br>";
+			} 
+			else {
+				raise_upload_error("Error uploading file: <b>". $_FILES['upfile']['tmp_name'] . "</b>");
+			}
+
+		}
+		else {
+			echo "<font style='color:red'>";
+			echo "Unauthorized access to this page.";
+		}
+	?>
+	</div>
+</body>
+</html>
---
+++ UploadTest/uf_cfg.php
@@ -0,0 +1,6 @@
+<?php
+	$max_file_size = 10; // Размера в KB !
+	//$upload_path = пътя до папката
+	$upload_slash = "\\"; 
+	
+?>
