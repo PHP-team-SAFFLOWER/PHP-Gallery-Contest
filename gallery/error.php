62db236926a99f33e5537b7e1dba300ca3750ae9
morsky <vladvv2m@gmail.com>
2014-08-27 13:55:57 +0300

    added error.php and edited register.css

---
+++ gallery/error.php
@@ -0,0 +1,21 @@
+<?php
+
+$title = "Error";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="CSS/register.css">
+</head>
+<body>
+<section>
+    <div class="user_interface">
+        <p>Error:</p>
+    </div>
+</section>
+</body>
+</html>
